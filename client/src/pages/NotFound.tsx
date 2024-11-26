134cb3fed44a4ec1a4c8ad1ae6252df4e0af21b8
Kevin <dennisk.blas@gmail.com>
2024-11-25 23:21:27 -0500

    componente not found y loadRotate

---
+++ client/src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NotFound: React.FC = () => {
+    return (
+        <div className='text-4xl text-purple-300'>
+            NotFound
+        </div>
+    )
+}
