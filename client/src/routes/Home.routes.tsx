27c7c072f15c8a73acb487a50eedb2aea2aa4866
Kevin <dennisk.blas@gmail.com>
2024-11-29 12:03:50 -0500

    rutas para el frontend

---
+++ client/src/routes/Home.routes.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { TasksProvider } from '../context/TasksContext'
+import { Home } from '../pages/Home'
+import { Navbar } from '../components/Menu/Navbar'
+import { NotFound } from '../pages/NotFound'
+
+export const HomeRoutes: React.FC = () => {
+    return (
+        <TasksProvider >
+            <Routes>
+                <Route path='' element={<Navbar />}>
+                    <Route index element={<Home />} />
+                </Route>
+                <Route path='/*' element={<NotFound />} />
+            </Routes>
+        </TasksProvider>
+    )
+};
--- client/src/routes/Profile.routes.tsx
+++ client/src/routes/Profile.routes.tsx
@@ -2,21 +2,21 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Profile } from '../pages/Profile'
 import { NotFound } from '../pages/NotFound'
+import { TasksProvider } from '../context/TasksContext'
 import { AuthProvider } from '../context/AuthContext'
+import { Navbar } from '../components/Menu/Navbar'
 
 export const ProfileRoutes: React.FC = () => {
     return (
         <AuthProvider>
-            <Routes>
-                <Route 
-                    path=''
-                    element={<Profile />}
-                />
-                <Route
-                    path='*'
-                    element={<NotFound />}
-                />
-            </Routes>
+            <TasksProvider>
+                <Routes>
+                    <Route path='' element={<Navbar />}>
+                        <Route path='' element={<Profile />} />
+                    </Route>
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
+            </TasksProvider>
         </AuthProvider>
     )
 }
