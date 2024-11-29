387c217708f6a43dbdd40deeff327bf984e332a0
Kevin <dennisk.blas@gmail.com>
2024-11-29 12:03:27 -0500

    nuevas interfaces para organizacion

---
+++ client/src/interfaces/auth/SectionFormProps.ts
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+export interface SectionFormProps{
+    children: ReactNode,
+    styles?: string,
+    isLogin?: boolean
+}
---
+++ client/src/interfaces/home/Features.ts
@@ -0,0 +1,8 @@
+export interface Feactures {
+    feature?: string,
+    styleSubtitle?: string,
+    stylesItems?: string,
+    subtitle?: string,
+    items?: string[],
+    listStyle?: string,
+}
