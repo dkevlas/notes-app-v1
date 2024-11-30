11bf8d8412f162981f1b23fbd3eb2b51074df22b
Kevin <dennisk.blas@gmail.com>
2024-11-29 21:01:52 -0500

    update .env

--- server/src/config.js
+++ server/src/config.js
@@ -2,8 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-    PORT: process.env.PORT,
-    HOST: process.env.HOST,
+    PORT: process.env.PORT || 3005,
+    HOST: process.env.HOST || '0.0.0.0',
     MONGO_URI: process.env.MONGO_URI,
     TOKEN_SECRET: process.env.TOKEN_SECRET,
     URL_FRONTEND: process.env.URL_FRONTEND,
